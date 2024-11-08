72e35417616017f10b3f47a4022684979ab7d382
ehsansdr <sehsan251@gmail.com>
2024-11-08 12:06:39 +0330

    sql excersise

--- src/main/resources/SQLs/sql_excersie.sql
+++ src/main/resources/SQLs/sql_excersie.sql
@@ -18,4 +18,20 @@ from matches team
 */
 
 ----------------------------------------------------------------------------------------------------
+-- https://techtfq.com/blog/30daysqlquerychallenge-query-1
+-- Q1
+with cte as (select * ,
+            case
+                when brand1 < brand2 then concat(brand1,brand2,year)
+                else concat(brand2,brand1,year)
+            end as pair_id
+            from brands),
+    cte_rn as (
+    select * , row_number() over(partition by pair_id order by pair_id) as rn
+    from cte
+    )
+select *
+from cte_rn
+where rn = 1
+   or (custom1 <> custom3 and custom2 <> custom4)
 
